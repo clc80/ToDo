449a2f6acd1f484f0ed10f6f2df80c9a8c50955f
Claudia Maciel <clc_80@yahoo.com>
2021-02-22 23:08:58 -0800

    Connected Storyboard with ViewController Code

--- ToDo/ToDoViewController.swift
+++ ToDo/ToDoViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ToDoViewController: UIViewController {
-
+    
+    @IBOutlet var addItemTextField: UITextField!
+    @IBOutlet var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet var toDoTable: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func AddButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
