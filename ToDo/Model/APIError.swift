c7bc24196834b98ac095478a18a6924537409e31
Claudia Maciel <clc_80@yahoo.com>
2021-02-27 21:19:41 -0800

    Added Models for Incoming Data

---
+++ ToDo/Model/APIError.swift
@@ -0,0 +1,12 @@
+//
+//  APIError.swift
+//  ToDo
+//
+//  Created by Claudia Maciel on 2/27/21.
+//
+
+import Foundation
+
+struct APIError: Codable {
+    var message: String
+}
---
+++ ToDo/Model/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDo
+//
+//  Created by Claudia Maciel on 2/27/21.
+//
+
+import Foundation
+
+struct ToDo: Codable {
+    var item: String
+    var priority: Int
+}
